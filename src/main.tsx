b25de0189c75aafaa3c260b8bcfc9dd96222f5f0
workbeamcontact-design <Workbeamcontact@gmail.com>
2025-11-25 01:51:05 +0000

    Merge src folder into root - keep config files and code together

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/globals.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  import { createRoot } from "react-dom/client";
+  import App from "./App.tsx";
+  import "./index.css";
+
+  createRoot(document.getElementById("root")!).render(<App />);
+  
