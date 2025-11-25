c344a0ddab7f666fe5bac07e34354ea7bdda7c21
workbeamcontact-design <Workbeamcontact@gmail.com>
2025-11-25 00:29:07 +0000

    Add missing build configuration files (tsconfig, vite.config, index.html, main.tsx)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/globals.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  import { createRoot } from "react-dom/client";
+  import App from "./App.tsx";
+  import "./index.css";
+
+  createRoot(document.getElementById("root")!).render(<App />);
+  
